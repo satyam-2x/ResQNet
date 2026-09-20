ca599d0b670a21785f4baa80d82ca552762756b9
Satyam kumar Ray <satyamkumarroy216@gmail.com>
2026-09-20 15:34:10 +0530

    Add ResQNet logo

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const app = express();
 
 app.use(cors({
     origin: [
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://main.dxseivhko5r62.amplifyapp.com/"
     ],
     credentials: true
 }));
