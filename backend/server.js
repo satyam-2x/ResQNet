39eb35a1f2fd9198ca4a0d65183689f5d8874320
Satyam kumar Ray <satyamkumarroy216@gmail.com>
2026-09-20 12:30:07 +0530

    Prepare backend for AWS Lambda deployment

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,14 @@ app.use('/api/v1', incidentRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
+if(require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    })
+} 
+
+module.exports = app;
